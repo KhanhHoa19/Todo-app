da3904ae1f46d8cf63d50b94a63398bbd16997f4
Hoa <hk2056951@gmail.com>
2024-09-20 20:51:24 +0700

    5> Adding curd to the js

--- app.js
+++ app.js
@@ -8,13 +8,29 @@ const addTask = ()=>
        if(text)
        {
             task.push({text: text , completed: false});
-            
+            taskInput.value ="";
             updateTaskList();
        }
        
 };
 
-        const updateTaskList = ()=>
+    const toggleTaskComplete =(index)=>{
+        tasks[index].completed = !task[index].completed;
+
+    }
+    const deleteTask =(index)=>
+    {
+        task.splice(index,1);
+        updateTaskList();
+    }
+    const editTask =(index)=>{
+        const taskInput =document.getElementById('taskInput');
+        taskInput.value= tasks[index].text
+
+        task.splice(index,1);
+        updateTaskList();
+    }
+        const updateTaskList = ()=> 
         {
             const taskList = document.getElementById('task-list');
             taskList.innerHTML =''
@@ -25,12 +41,13 @@ const addTask = ()=>
                 listItem.innerHTML=`
                 <div class=""taskItem> 
                     <div class ="task>
-                        <input type ="checkbox" class ="checkbox" />
-                            <p> Finish this project </p>
+                        <input type ="checkbox" class ="checkbox" ${task.completed ? "checked" :""} />
+                        
+                            <p> ${task.text} </p>
                     </div>
                     <div class ="icons>
-                        <img src ="delete.png"/>
-                        <img src ="edit.png"/>
+                        <img src ="delete.png" onClick="deleteTask(${index})/>
+                        <img src ="edit.png" onClick="editTask(${index})/>
                     </div>                   
                 </div>
                 `;
@@ -39,6 +56,6 @@ const addTask = ()=>
             });
         };
 document.getElementById('newTask').addEventListener('click', function(e){
-    e.preventDefault()
+    e.preventDefault();
     addTask();
 })
